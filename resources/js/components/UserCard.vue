cdf054472aa363c45e4945e7a2453e978afb05f3
Jalique Gordon <jaliquegordon@gmail.com>
2024-04-28 14:38:38 -0400

    Added UserCard.vue

---
+++ resources/js/components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+<div class = "user-card">
+   <img :src = "user.photo" alt = "Profile Picture">
+
+   div class "user-name"
+       <h2>{{ user.name }}</h2>
+       <p>@{{ user.name }}</p>
+   </div>
+
+   <div class = "user-details">
+        <p>Email:: {{ user.email }}</p>
+        <p>Location:: {{ user.location }}</p>
+        
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: Object
+    }
+}
+</script>
