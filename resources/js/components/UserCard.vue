46a40b0b5b690e6f0b7c8fba60251ccb1407fcbc
Jalique Gordon <jaliquegordon@gmail.com>
2024-04-28 18:53:23 -0400

    Made modifications to UserCard.vue

--- resources/js/components/UserCard.vue
+++ resources/js/components/UserCard.vue
@@ -2,9 +2,13 @@
 <div class = "user-card">
    <img :src = "user.photo" alt = "Profile Picture">
 
-   div class "user-name"
+   <div class "user-name">
        <h2>{{ user.name }}</h2>
        <p>@{{ user.name }}</p>
+   </div>
+
+   <div class = "user-biography">
+      <p>{{ user.biography }}</p>
    </div>
 
    <div class = "user-details">
@@ -18,7 +22,33 @@
 <script>
 export default {
     props: {
-        user: Object
+        user_id: Number
+    },
+    data() {
+        return {
+            user: null
+        };
+    },
+    mounted(){
+        this.fetchUser(this.user_id);
+    },
+
+    methods: {
+        fetchUser(user_id) {
+            axios.get('api/users/${user_id}')
+                 .then(response => {
+                    this.user = response.data;
+                 })
+                 .catch(error => {
+                    console.error('Error fetching user:', error);
+
+                 });
+
+
+        }
     }
+
 }
 </script>
+
+
