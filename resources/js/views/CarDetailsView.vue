ba77d868b4f7ca5190652e49f6a679cdaf07a44a
b13nmaN <blenmanjoel@gmail.com>
2024-04-29 15:52:35 -0400

    update

--- resources/js/views/CarDetailsView.vue
+++ resources/js/views/CarDetailsView.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class="container w-50  mt-5" >
-    <div v-if="car" class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img :src="'/storage/car_photos/' + car.photo" class="img-fluid rounded-start" :alt="car.make">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <div class="d-flex flex-row mb-3">
-              <div>
-                <p class="card-text">{{ car.year }} {{ car.make }}</p>
-                <p class="card-text">{{ car.model }}</p>
-                <p class="card-text">{{ car.description }}</p>
-                <p class="card-text">Color: {{ car.colour }}</p>
-                <p class="card-text">Body Type: {{ car.car_type }}</p>
-                <p class="card-text">Price: {{ car.price }}</p>
-                <p class="card-text">Transmission: {{ car.transmission }}</p>
-              </div>
-              <div>
-                <button class="btn btn-primary">Button</button>
+    <div class="container w-50 mt-5" >
+        <div v-if="car" class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img :src="'/storage/car_photos/' + car.photo" class="img-fluid rounded-start" :alt="car.make">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <div class="d-flex flex-column mb-3">
+                  <div>
+                    <p class="card-title fs-1 fw-bold mb-0">{{ car.year }} {{ car.make }}</p>
+                    <p class="card-text fs-2 fw-bold text-body-secondary">{{ car.model }}</p>
+                    <p class="card-text fw-medium">{{ car.description }}</p>
+                    <div class="row">
+                      <div class="col">
+                        <p class="card-text fw-medium">Color {{ car.colour }}</p>
+                      </div>
+                      <div class="col">
+                        <p class="card-text fw-medium">Body Type {{ car.car_type }}</p>
+                      </div>
+                    </div>
+                    <div class="row mb-3">
+                      <div class="col">
+                        <p class="card-text fw-medium">Price {{ car.price }}</p>
+                      </div>
+                      <div class="col">
+                        <p class="card-text fw-medium">Transmission {{ car.transmission }}</p>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  <div>
+                    <button class="btn btn-primary">Button</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
-  </div>
   </template>
   
   <script setup>
